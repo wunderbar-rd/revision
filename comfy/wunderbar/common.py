668ad73c3c04a5dded3758803b7fc2ce6fb45076
Jerven Clark Chua <jervenclark@gmail.com>
2024-04-10 20:41:28 +0800

    Add change clothing pipeline
    Signed-off-by: Jerven Clark Chua <jervenclark@gmail.com>

--- comfy/wunderbar/common.py
+++ comfy/wunderbar/common.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append('../..')
+
 from typing import Sequence, Mapping, Any, Union
 
 def get_value_at_index(obj: Union[Sequence, Mapping], index: int) -> Any:
@@ -22,3 +26,25 @@ def get_value_at_index(obj: Union[Sequence, Mapping], index: int) -> Any:
         return obj[index]
     except KeyError:
         return obj["result"][index]
+    
+def import_custom_nodes() -> None:
+    """Find all custom nodes in the custom_nodes folder and add those node objects to NODE_CLASS_MAPPINGS
+
+    This function sets up a new asyncio event loop, initializes the PromptServer,
+    creates a PromptQueue, and initializes the custom nodes.
+    """
+    import asyncio
+    import execution
+    from nodes import init_custom_nodes
+    import server
+
+    # Creating a new event loop and setting it as the default loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Creating an instance of PromptServer with the loop
+    server_instance = server.PromptServer(loop)
+    execution.PromptQueue(server_instance)
+
+    # Initializing custom nodes
+    init_custom_nodes()
